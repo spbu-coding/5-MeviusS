9f3fe54d94015f504210f3beb484e1072bc187e6
MeviusS <st090929@student.spbu.ru>
2020-11-17 19:37:45 +0300

    first commit

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+void input(){
+    char buffer[5];
+    gets(buffer);
+    fflush(stdout);
+}
+
+void other(){
+    printf("Dodik \n");
+}
+
+int main(){
+    printf("%p \n", &other);
+    input();
+    printf("Normal output \n");
+
+    return 0;
+}
